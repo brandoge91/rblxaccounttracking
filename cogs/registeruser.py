ca910f34b4b57c34192f8289f0ad810ff98b4293
CookieHax <noah.hafner@outlook.com>
2023-07-30 11:09:43 +0100

    Class docstring created.

--- cogs/registeruser.py
+++ cogs/registeruser.py
@@ -4,14 +4,15 @@ from disnake.ext import commands
 
 
 class registeruser(commands.Cog):
+    """
+    Register users with the run of a command, not currently role locked.
+    """
     def __init__(self, bot):
         self.bot = bot
 
     @commands.slash_command(description="Register a user for tracking.")
     async def registeruser(self, inter: disnake.ApplicationCommandInteraction, userid: int):
         await inter.response.defer(ephemeral=True)
-        NewUser = dbmanager.user(userid)
-        NewUser.writeData(minutes=0, lastOnline="0000-00-00T00:00:00.0Z", lastLocation="Unknown")
         await inter.edit_original_message(f"We're now tracing {userid}!")
 
 
