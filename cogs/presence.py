61c9508300c77ae2402db18fbebcf556be48ee00
CookieHax <noah.hafner@outlook.com>
2023-07-30 23:19:20 +0100

    Automatic update

--- cogs/presence.py
+++ cogs/presence.py
@@ -1,24 +1,23 @@
 import disnake
-from disnake.ext import commands
-
+from disnake.ext import commands, tasks
 
 class PresenceCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.update_presence.start()
 
     def get_server_count(self):
         return len(self.bot.guilds)
 
-    @commands.Cog.listener()
-    async def on_ready(self):
+    @tasks.loop(minutes=10)
+    async def update_presence(self):
         server_count = self.get_server_count()
         activity_name = f"Account Tracking | {server_count} servers"
-        await self.bot.change_presence(
-            activity=disnake.Activity(
-                name=activity_name
-            )
-        )
+        await self.bot.change_presence(activity=disnake.Activity(name=activity_name))
 
+    @update_presence.before_loop
+    async def before_update_presence(self):
+        await self.bot.wait_until_ready()
 
 def setup(bot):
     bot.add_cog(PresenceCog(bot))
