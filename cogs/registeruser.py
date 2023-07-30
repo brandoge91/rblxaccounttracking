8a25011ade74b1fadaa1f25f7f70c0e2b1d6df91
CookieHax <noah.hafner@outlook.com>
2023-07-30 11:10:59 +0100

    Line to long.

--- cogs/registeruser.py
+++ cogs/registeruser.py
@@ -7,11 +7,16 @@ class registeruser(commands.Cog):
     """
     Register users with the run of a command, not currently role locked.
     """
+
     def __init__(self, bot):
         self.bot = bot
 
     @commands.slash_command(description="Register a user for tracking.")
-    async def registeruser(self, inter: disnake.ApplicationCommandInteraction, userid: int):
+    async def registeruser(
+        self,
+        inter: disnake.ApplicationCommandInteraction,
+        userid: int
+    ):
         await inter.response.defer(ephemeral=True)
         await inter.edit_original_message(f"We're now tracing {userid}!")
 
