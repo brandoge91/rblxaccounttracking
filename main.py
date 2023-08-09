0b1a6914caf9ff876a02d958a8a6075611464d3d
brandon <brandoge91@gmail.com>
2023-08-08 20:58:38 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,6 @@ import os
 import disnake
 from disnake.ext import commands
 
-import dotenv
-
-
-ENV = dotenv.load_dotenv()
-
 
 intent = disnake.Intents.default()
 bot = commands.InteractionBot(intents=intent)
